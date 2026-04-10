bb250442029d37032c1cf7ed2549b1cef94ec9cd
Iain Dillingham <iain@dillingham.me.uk>
2026-04-10 17:27:02 +0100

    Tidy up `publishconf.py`

--- publishconf.py
+++ publishconf.py
@@ -1,6 +1,3 @@
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
 
@@ -9,8 +6,6 @@ from pelicanconf import *  # noqa: F403
 
 sys.path.append(os.curdir)
 
-
-# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://www.dillingham.me.uk"
 RELATIVE_URLS = False
 
@@ -18,8 +13,3 @@ FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-# DISQUS_SITENAME = ""
-# GOOGLE_ANALYTICS = ""
